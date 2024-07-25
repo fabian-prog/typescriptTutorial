c703760af131ccc8791afd7a13d5a1a671b9cef6
fabian-prog <fabianrobin2001@gmail.com>
2024-07-25 23:35:34 +0530

    "first class of typescript"

---
+++ 01_Tsx/intro.js
@@ -0,0 +1,6 @@
+var user = {
+    name: "fabian",
+    age: 10,
+};
+console.log("name", "fabian");
+console.log(user.name);
---
+++ 01_Tsx/intro.tsx
@@ -0,0 +1,8 @@
+var user={
+   name:"fabian",
+   age:10, 
+}
+
+console.log("name", "fabian");
+console.log(user.name);
+
